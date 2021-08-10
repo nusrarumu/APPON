8e2961242f67e0105a2d182297efbcd94e82fb91
nusrarumu <79602260+nusrarumu@users.noreply.github.com>
2021-08-10 23:08:00 +0600

    html css landing page

---
+++ assets/js/main.js
@@ -0,0 +1,95 @@
+$(document).ready(function () {
+
+
+    
+    $('#full-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    
+    $('.screenshots-slick').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+    });
+
+    $('.video-play').venobox();
+
+    $('.feedback-content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        asNavFor: '.client-slick'
+      });
+      $('.client-slick').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.feedback-content',
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true
+      });
+
+})
